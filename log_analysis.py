35de19277eb9b88c9d026daa3e15fbeb0a9a6f09
vagrant <vagrant@vagrant.vm>
2019-03-23 12:33:01 +0000

    wrote sql query to return answer 1 and created function to print the answers correctly

--- log_analysis.py
+++ log_analysis.py
@@ -4,17 +4,35 @@ import psycopg2
 
 DBNAME = "news"
 
-# query_1 =
+query_1 = """select title, count(*) as views from articles
+             join log on articles.slug = substring(log.path, 10)
+             group by title order by views DESC limit 3;"""
 
 # query_2 =
 
 # query_3 =
 
 def connect_to_db():
+
     conn = psycopg2.connect(database=DBNAME)
     cursor = conn.cursor()
-    cursor.execute("select * from articles")
+    cursor.execute(query_1)
     results = cursor.fetchall()
-    print(results)
     conn.close
-connect_to_db()
+    return results
+
+
+
+
+def print_answers(results):
+
+    print('-------------')
+    for result, view in results:
+        print(result, '--', view)
+
+
+
+
+def main():
+    print_answers(connect_to_db())
+main()
